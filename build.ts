b75a1cc5d3a5513afdf2f289696d7901b42f239a
neverm1ndo <it@nmnd.ru>
2021-03-09 20:35:44 +0300

    update builder

--- build.ts
+++ build.ts
@@ -4,23 +4,45 @@
 
 import * as fs from 'fs-extra';
 import Logger from 'jet-logger';
+import { join } from 'path';
 import * as childProcess from 'child_process';
 
 // Setup logger
 const logger = new Logger();
 logger.timestamp = false;
 
-
-
-
 (async () => {
     try {
         // Remove current build
+        logger.info(`[builder]: clear ./dist directory`);
         await remove('./dist/');
         // Copy production env and conf file
+        logger.info(`[builder]: copy production env and conf file`);
         await copy('./src/packages/freeroam/environment/production.env', './dist/packages/freeroam/environment/production.env');
         await copy('./src/packages/freeroam/conf.json', './dist/packages/freeroam/conf.json');
+        // Copy ingame config files
+        await (async () => {
+          const confDir = './packages/freeroam/configs';
+          await (async () => {
+            fs.readdir(join('./src/', confDir), async (err: NodeJS.ErrnoException) => {
+              if (err) {
+                logger.info(`[builder]: creating configs directory ${confDir}`);
+                await fs.mkdir(join('./dist/', confDir));
+              } else {
+                logger.info(`[builder]: ${confDir} exists`);
+              }
+            });
+          })();
+          fs.readdir(join('./src/', confDir), (err: NodeJS.ErrnoException, configs: string[]) => {
+            if (err) throw err;
+            configs.forEach(async file => {
+              await copy(join('./src/',confDir, file), join('./dist/', confDir, file));
+              logger.info(`[builder]: copy ingame config ${file}`);
+            });
+          });
+        })();
         // Copy back-end files
+        logger.info(`[builder]: copy back-end files`);
         await exec('tsc --build tsconfig.prod.json', './')
     } catch (err) {
         logger.err(err);
