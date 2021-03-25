9c3e54b285509ba2511a5ab3502ff50238ef7f9e
neverm1ndo <it@nmnd.ru>
2021-03-25 06:08:02 +0300

    fix events in cli_packages

--- src/client_packages/freeroam/core/events.ts
+++ src/client_packages/freeroam/core/events.ts
@@ -36,30 +36,30 @@ export default function(menu: any) {
     mp.events.add('cefData', function() {
         // CEF data.
         if (arguments[0] !== 'client_color')
-            mp.events.callRemote('clientData', JSON.stringify(arguments));
+            mpex.events.callRemote('clientData', JSON.stringify(arguments));
         // Vehicle color or neon.
         else {
             let color = JSON.parse(arguments[2]);
             switch (arguments[1]) {
             // Primary color.
             case 'primary':
-                mp.players.local.vehicle.setCustomPrimaryColour(color.r, color.g, color.b);
+                mpex.players.local.vehicle.setCustomPrimaryColour(color.r, color.g, color.b);
                 break;
             // Secondary color.
             case 'secondary':
-                mp.players.local.vehicle.setCustomSecondaryColour(color.r, color.g, color.b);
+                mpex.players.local.vehicle.setCustomSecondaryColour(color.r, color.g, color.b);
                 break;
             // Neon.
             case 'neon':
                 // If vehicle neon disabled - enable it.
-                if (!mp.players.local.vehicle.isNeonLightEnabled(0)) {
+                if (!mpex.players.local.vehicle.isNeonLightEnabled(0)) {
                     for (let i = 0; i < 4; i++) {
-                        mp.players.local.vehicle.setNeonLightEnabled(i, true);
+                        mpex.players.local.vehicle.setNeonLightEnabled(i, true);
                     }
                 }
 
                 // Set neon color.
-                mp.players.local.vehicle.setNeonLightsColour(color.r, color.g, color.b);
+                mpex.players.local.vehicle.setNeonLightsColour(color.r, color.g, color.b);
                 break;
             }
         }
